479c7354b8047ac250e802fc4dcbfcbaa654b596
ngt22 <ngt22@me.com>
2015-08-12 14:58:52 +0900

    Add user micropost

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,3 @@
-#require 'spec_helper'
-#
-#describe "StaticPages" do
-#  describe "GET /static_pages" do
-#    it "works! (now write some real specs)" do
-#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#      get static_pages_index_path
-#      response.status.should be(200)
-#    end
-#  end
-#end
 require 'spec_helper'
 
 describe "Static pages" do
@@ -27,6 +16,22 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
 
     it { should_not have_title('| Home') }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+           expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
